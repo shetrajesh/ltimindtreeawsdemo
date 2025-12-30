4d1c135eef2eb10c06ceab2841a3c999ef445e96
rshet007@yahoo.com <rshet007@yahoo.com>
2025-12-30 09:28:14 +0530

    first commit

---
+++ src/main/java/com/examly/ltimindtreeawsdemo/LtimindtreeawsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.examly.ltimindtreeawsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtimindtreeawsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtimindtreeawsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examly/ltimindtreeawsdemo/LtimindtreeawsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.ltimindtreeawsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtimindtreeawsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
